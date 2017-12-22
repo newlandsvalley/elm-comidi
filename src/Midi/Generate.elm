88d17f3b1776713b2115931e0e4f583294f26dd5
Richard Alex Hofer <rofer@google.com>
2017-12-22 10:03:00 -0500

    Start on generating midi files.

--- src/Midi/Generate.elm
+++ src/Midi/Generate.elm
@@ -1,4 +1,4 @@
-module Midi.Generate exposing (event, events, recording)
+module Midi.Generate exposing (event, recording)
 
 {-| Library for encoding MIDI types as "binary"
 
@@ -9,7 +9,7 @@ module Midi.Generate exposing (event, events, recording)
 
 -}
 
-import Bitwise
+import Bitwise exposing (shiftRightBy)
 import Char
 import Midi.Types exposing (..)
 
@@ -21,22 +21,22 @@ event event =
             0xF0 :: (bytes ++ [ 0xF7 ])
 
         NoteOn channel note velocity ->
-            [ 144 + channel, note, velocity ]
+            [ 0x90 + channel, note, velocity ]
 
         NoteOff channel note velocity ->
-            [ 128 + channel, note, velocity ]
+            [ 0x80 + channel, note, velocity ]
 
         NoteAfterTouch channel note velocity ->
-            [ 160 + channel, note, velocity ]
+            [ 0xA0 + channel, note, velocity ]
 
         ControlChange channel controllerNumber value ->
-            [ 176 + channel, controllerNumber, value ]
+            [ 0xB0 + channel, controllerNumber, value ]
 
         ProgramChange channel value ->
-            [ 192 + channel, value ]
+            [ 0xC0 + channel, value ]
 
         ChannelAfterTouch channel velocity ->
-            [ 208 + channel, velocity ]
+            [ 0xD0 + channel, velocity ]
 
         PitchBend channel bend ->
             let
@@ -44,24 +44,65 @@ event event =
                     Bitwise.and bend 127
 
                 upper =
-                    Bitwise.shiftRightBy 7 bend
+                    shiftRightBy 7 bend
             in
-                [ 224 + channel, lower, upper ]
+                [ 0xE0 + channel, lower, upper ]
 
         _ ->
             Debug.crash "TODO"
 
 
-
--- TODO(rofer): events should later automatically use Running Status where
--- possible
-
-
-events : List MidiEvent -> List Byte
-events events =
-    List.concatMap event events
-
-
 recording : MidiRecording -> List Byte
-recording ( header, tracks ) =
-    Debug.crash "TODO" ()
+recording ( h, tracks ) =
+    header h ++ (List.concatMap track tracks)
+
+
+
+-- Lower level generators
+
+
+header : Header -> List Byte
+header h =
+    let
+        format =
+            h.formatType
+
+        numTracks =
+            h.trackCount
+
+        division =
+            h.ticksPerBeat
+    in
+        (strToBytes "MThd") ++ [ 0, 0, 0x06, format, numTracks, division ]
+
+
+track : Track -> List Byte
+track t =
+    strToBytes "MTrk"
+
+
+
+-- Helper functions
+
+
+strToBytes : String -> List Byte
+strToBytes =
+    (List.map Char.toCode) << String.toList
+
+
+uint32 : Int -> List Byte
+uint32 x =
+    let
+        b1 =
+            Bitwise.and 255 x
+
+        b2 =
+            Bitwise.and 255 (shiftRightBy 8 x)
+
+        b3 =
+            Bitwise.and 255 (shiftRightBy 16 x)
+
+        b4 =
+            Bitwise.and 255 (shiftRightBy 24 x)
+    in
+        [ b1, b2, b3, b4 ]
